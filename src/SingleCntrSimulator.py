0a04ccb834e683c2ad548ff8a18918eb0af404dc
ofanan <ofanan@gmail.com>
2024-04-25 17:37:09 +0300

    reg

--- src/SingleCntrSimulator.py
+++ src/SingleCntrSimulator.py
@@ -635,8 +635,8 @@ def main ():
         #            signed       = False # When True, assume an additional bit for the  
         #            )
         hyperSize  = 2
-        for cntrSize in [8]:
-            simController = SingleCntrSimulator (verbose = [settings.VERBOSE_RES]) #settings.VERBOSE_RES, settings.VERBOSE_PCL],)
+        for cntrSize in [8, 10, 12, 14, 16]:
+            simController = SingleCntrSimulator (verbose = [settings.VERBOSE_RES, settings.VERBOSE_PCL]) #settings.VERBOSE_RES, settings.VERBOSE_PCL],)
             simController.runSingleCntr \
                 (dwnSmple       = False,  
                  # modes          = ['F2P_li_h2', 'Morris', 'CEDAR'], #, 'SEAD_stat_e3', 'F2P_li', 'Morris', 'CEDAR'], #[],
@@ -644,7 +644,7 @@ def main ():
                  cntrSize       = cntrSize, 
                  hyperSize      = hyperSize,
                  numOfExps      = 50,
-                 rel_abs_n      = False,
+                 rel_abs_n      = True,
                  erTypes        = ['RdRmse'], # The error modes to gather during the simulation. Options are: 'WrEr', 'WrRmse', 'RdEr', 'RdRmse' 
             )
         
