47ea9db245fe6cec8c11c59b146c4c430c37b33f
ofanan <ofanan@gmail.com>
2024-06-18 11:58:51 +0300

    regarge

--- src/CountMinSketch.py
+++ src/CountMinSketch.py
@@ -443,7 +443,9 @@ if __name__ == '__main__':
     try:
         cntrSize = 8
         for width in [2**i for i in range (10, 19)]:
-            for mode in ['SEAD_stat_e2', 'SEAD_stat_e3', 'SEAD_stat_e4']:    
+            for mode in ['SEAD_stat_e2']:    
+            # for mode in ['SEAD_stat_e3']:    
+            # for mode in ['SEAD_stat_e4']:    
             # for mode in ['F2P_li_h2']:    
             # for mode in ['F3P_li_h3']:    
             # for mode in ['F2P_lli', 'CEDAR', 'Morris']:    
