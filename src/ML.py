2489076d763463679f364dba47418c87165f773f
ofanan <ofanan@gmail.com>
2024-08-13 13:12:21 +0300

    rg

--- src/ML.py
+++ src/ML.py
@@ -49,12 +49,6 @@ def compute_cost_my (X, y, w, b):
     w = tf.convert_to_tensor (w, dtype=myDtype)
     m = X.shape[0]
     prediction = predict (X, w, b)
-    costVec = tf.math.square(prediction-y)
-    # print (f'prediction={prediction}\ny={y}\ncostVec={costVec}')
-    # exit ()
-    # cost = tf.reduce_sum(input_tensor=costVec)
-    # print (f'cost={cost}')
-    # exit ()
     return tf.reduce_sum(input_tensor=tf.math.square(prediction-y)) / (2*m)
 
 X = np.array([[2104, 5, 1, 45], [1416, 3, 2, 40], [852, 2, 1, 35]])
@@ -62,18 +56,6 @@ y = np.array([460, 232, 178])
 b_init = 785.1811367994083
 w_init = np.array([ 0.29133535, 18.75376741, -53.36032453, -26.42131618])
 
-# X = tf.tile (tf.constant([[1, 2, 3]]), [2,1])
-# X = tf.constant ([[1, 2, 3], [4, 5, 6]])
-# w = tf.constant ([1, 1, 1])
-# ww = tf.tile ([w], [2, 1])
-# prediction = tf.reduce_sum(input_tensor=tf.math.multiply (X, ww), axis=1)
-# print (f'X={X}\nw={w}\nww={ww}\nprediction={prediction}')
-# exit ()
-# numExamples = X.shape[0]
-# wMat = tf.tile ([w], [numExamples, 1])
-# prediction = tf.math.multiply(X, wMat)
-# error (prediction)
-  
 costLab = compute_cost_lab(X=X, y=y, b=b_init, w=w_init)  
 costMy  = compute_cost_my (X=X, y=y, b=b_init, w=w_init)  
 print (f'costLab={costLab}, costMy={costMy}')
