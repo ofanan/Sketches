4c25ca479ff3a496c59ba84fb969ab42003ca79f
ofanan <ofanan@gmail.com>
2024-11-17 20:22:30 +0200

    areer

--- src/CountMinSketch.py
+++ src/CountMinSketch.py
@@ -520,9 +520,9 @@ def runMultiProcessSim ():
   
 if __name__ == '__main__':
     try:
-        mode = 'Morris' 
+        mode = 'CEDAR' 
         for traceName in ['Caida2']: #['Caida2']: #, 'Caida2']: 
-            for width in [2**i for i in range (14, 19)]:   
+            for width in [2**i for i in range (10, 15)]:   
                 LaunchCmsSim (
                     traceName   = traceName,
                     cntrSize    = 8,
